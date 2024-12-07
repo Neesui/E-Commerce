d323ba7c69b4049e49c2d9aa54c6b51f0982594d
Neesui <nanumaya2012@gmail.com>
2024-12-07 13:39:15 +0545

    add product page and register page

--- frontend/src/Myroute.jsx
+++ frontend/src/Myroute.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import required components
 import Layout from "./components/userComponent/Layout";
 import Homepage from "./pages/userpages/Homepage";
+import Product from "./pages/userpages/Product";
+import Login from './pages/userpages/Login';
 
 const Myroute = () => {
   return (
@@ -9,6 +11,9 @@ const Myroute = () => {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Homepage />} />
+          <Route path="product" element={<Product />} />
+            <Route path="register" element={<Register/>}/>
+            <Route path="login" element={<Login/>}/>
         </Route>
       </Routes>
     </Router>
---
+++ frontend/src/pages/userpages/Product.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Cards from '../../components/usercomponents/Cards'
+
+const Product = () => {
+  return (
+    <>
+    <Cards/>
+    <Cards/>
+    </>
+  )
+}
+
+export default Product;
