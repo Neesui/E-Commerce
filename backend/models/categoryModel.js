993b8fcfc2bf31d3a74b2a28253cad93307dce5d
Neesui <nanumaya2012@gmail.com>
2024-12-08 22:47:23 +0545

    add backend code

---
+++ backend/models/categoryModel.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+// Define the schema for categories
+const categorySchema = new mongoose.Schema({
+
+    categoryName: {
+        type: String,
+        required: true,
+        unique:true,
+        trim: true // Removes extra spaces from beginning and end
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    imageUrl: {
+        type: String,
+        required: true,
+        trim: true
+    }
+}, { timestamps: true }); // Automatically manages 'createdAt' and 'updatedAt'
+
+// Export the model
+export default mongoose.model('Category', categorySchema);
